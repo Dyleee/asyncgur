2a0d68c41d9bdb6977187f1c9f3614232d0ea024
Oyindoubra Akposeye <36546002+Dyleee@users.noreply.github.com>
2019-04-08 16:59:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from distutils.core import setup
-with open('README.md') as f:
-    long_desc = f.read()
 
 setup(
     name='pygur',
     version='0.1',
     packages=['pygur',],
     license='MIT License',
-    install_requires=['aiohttp', 'dacite',]
-    long_description=long_desc,
+    install_requires=['aiohttp', 'dacite',],
+    long_description=open('README.md').read(),
     author='Dylee',
     author_email='dylee@mewbot.me',
 )
