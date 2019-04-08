5e5aa371812a33633df429c391a8d804d9c2dc96
Oyindoubra Akposeye <36546002+Dyleee@users.noreply.github.com>
2019-04-08 16:50:43 +0100

    Create pygur.py

---
+++ pygur.py
@@ -0,0 +1,82 @@
+import asyncio
+import json
+import dacite
+import aiohttp
+
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
+class Image_info:
+    id:str
+    name:Any
+    description:Any
+    datetime:Any
+    type:Any
+    animated:Any
+    width:Any
+    height:Any
+    size:Any
+    views:Any
+    bandwidth:Any
+    vote:Any
+    favorite:Any
+    nsfw:Any
+    section:Any
+    account_url:Any
+    account_id:Any
+    is_ad:Any
+    in_most_viral:Any
+    has_sound:Any
+    tags:Any
+    ad_type:Any
+    ad_url:Any
+    edited:Any
+    in_gallery:Any
+    deletehash:Any
+    name:Any
+    link:Any
+
+@dataclass
+class Response:
+    success:Any
+    status:Any
+    data:dict
+
+
+class Imgur:
+
+    def __init__(self, client_id):
+        self.client_id = client_id
+        self.headers = {
+        'Authorization' : f'Client-ID {self.client_id}'
+        }
+
+    async def get_request(self, url, payload=None):
+        async with aiohttp.request('get', url, headers=self.headers, data=payload) as resp:
+            resp.raise_for_status
+            return json.loads(await resp.text())
+
+    async def post_request(self, url, payload):
+        async with aiohttp.request('post', url, headers=self.headers, data=payload) as resp:
+            resp.raise_for_status
+            return json.loads(await resp.text())
+
+
+    async def get_image(self, image_hash):
+        response = await self.get_request(f'https://api.imgur.com/3/image/{image_hash}')
+        return response
+
+    async def upload_image(self, title=None, description=None, name=None, image=None, video=None):
+        payload = {
+        'title': title,
+        'description': description,
+        'name': name,
+        'image': image,
+        'video': video
+        }
+        response = await self.post_request(f'https://api.imgur.com/3/upload', payload)
+        print(response)
+        image_data = dacite.from_dict(data_class=Image_info, data=response['data'])
+        api_response = dacite.from_dict(data_class=Response, data=response)
+        return image_data, api_response
