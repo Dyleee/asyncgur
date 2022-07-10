53af1a830e94c89b847d6544a7e51547080dd177
Oyindoubra Akposeye <oyinxdoubx@gmail.com>
2022-07-10 15:24:48 +0100

    add gitignore

--- setup.py
+++ setup.py
@@ -4,9 +4,19 @@ setup(
     name='asyncgur',
     version='0.1.1',
     packages=['asyncgur',],
-    license='MIT License',
+    license='LGPLv3',
     install_requires=['aiohttp', 'dacite',],
     long_description=open('README.md').read(),
+    keywords=["asyncio", "imgur", "async", "aiohttp", "pil", "pillow", "image", "upload",],
     author='Dylee',
     author_email='eelyd@protonmail.com',
+    url="https://github.com/Dyleee/asyncgur",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
 )
